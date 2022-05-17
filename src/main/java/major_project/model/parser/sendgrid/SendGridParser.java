6a472b4263e32afafd191188c82af5904cf2bd8b
koduh5c <76833191+koduh5c@users.noreply.github.com>
2022-05-17 17:19:40 +1000

    Delete src directory

--- src/main/java/major_project/model/parser/sendgrid/SendGridParser.java
+++
@@ -1,7 +0,0 @@
-package major_project.model.parser.sendgrid;
-
-import major_project.model.parser.sendgrid.container.Email;
-
-public interface SendGridParser {
-    public boolean sendEmail(Email email);
-}
